24cc3241ca4566746a83a655ba86f560641357c8
Stary Cheng <portegestary@gmail.com>
2019-08-16 01:23:29 +0800

    test

--- app/Http/Controllers/PickController.php
+++ app/Http/Controllers/PickController.php
@@ -119,8 +119,7 @@ class PickController extends Controller
 	{
 		$client = new Google_Client();
         Storage::disk('local')->put('cred.json', file_get_contents(env('YOUTUBE_CRED_JSON')));
-        logger(Storage::disk('local')->getAdapter()->getPathPrefix() . 'cred.json');
-        logger(file_get_contents(env('YOUTUBE_CRED_JSON')));
+        dd(file_get_contents(env('YOUTUBE_CRED_JSON')));
         $client->setAuthConfig(Storage::disk('local')->getAdapter()->getPathPrefix() . 'cred.json');
         $client->useApplicationDefaultCredentials();
 		$client->addScope(Google_Service_YouTube::YOUTUBE_FORCE_SSL);
